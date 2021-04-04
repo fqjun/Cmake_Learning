dbd2c5a37e1d315e9376b616ee341e145dc14f77
fqjun <1767851382@qq.com>
2021-04-05 01:14:30 +0800

    Vmain 1、update file

---
+++ demo2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "./mymath.hpp"
+using namespace std;
+
+int main(int argc,char *argv[])
+{
+	if(argc<3)
+	{
+		cout<<"Usage:"<<argv[0]<<endl;
+		return 1;
+	}
+	double base =atof(argv[1]);
+	int exponent=atoi(argv[2]);
+	cout<<"result"<<endl<<power(base,exponent);
+	return 0;
+}
